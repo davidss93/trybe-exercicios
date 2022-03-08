8c73683b46977f36b4be7f834471fc2fb6735b81
David <davidss93@outlook.com>
2022-03-08 15:53:23 -0300

    utilizado for/of para imprimir nomes da lista

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-array-loop-for/script.js
@@ -1,14 +1,4 @@
-// var numero = 7;
-// for(var contador = 1; contador <= 9;  contador++) {
-//     console.log(numero * contador);
-// }
-
-// let cars = ['Saab', 'Volvo', 'BMW'];
-// for(let index = 0; index < cars.length; index += 1) {
-//     console.log(cars[index]);
-// }
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'melancia'];
-for(let index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let lista of names) {
+    console.log(lista);
 }
