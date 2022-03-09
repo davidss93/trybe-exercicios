4fe039c43537c5d6995ee91e94193973b7624586
David <davidss93@outlook.com>
2022-03-09 15:10:51 -0300

    teste

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-array-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-array-loop-for/script.js
@@ -1,2 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-    console.log(numbers);
+
+for(let index = 0; index < numbers.length; index += 1) {
+    soma = 0;
+    soma += numbers[index];
+    let media = soma / numbers.length;
+    if (index >= numbers.length){;
+    console.log(media);
+}
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-introducao-primeiros-passos/script.js
@@ -0,0 +1,12 @@
+const a = 2;
+const b = 3;
+
+a = 100;
+// a - b;
+// a * b;
+// a / b;
+// a % b;
+
+if (a > b) {
+    console.log("true")
+}
